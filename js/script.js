f65af70bf67ce28f088b08361edf66610e072d29
TienPham666 <2051052136tien@ou.edu.vn>
2021-09-13 18:17:35 +0700

    mmm

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#go-to-top").hide()
+    $(window).scroll(function() {
+       if ($(this).scrollTop() >= 435)
+            $("#go-to-top").show("slow")
+        else
+            $("#go-to-top").hide("slow")
+    })
+    $("#go-to-top").click(function() {
+        $("html, body").animate({
+            scrollTop:0
+        }, 1000);
+    })
+})
